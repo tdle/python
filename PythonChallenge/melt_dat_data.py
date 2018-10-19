f8e0f9c33967fc1b1146489fca18e98a165c8a21
malteserteresa <tess_the_titan@hotmail.com>
2018-10-19 19:43:00 +0200

    Provied solution for data transformation

---
+++ PythonChallenge/melt_dat_data.py
@@ -0,0 +1,11 @@
+"""
+    Transforms data into desired format
+"""
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+cols = df.columns
+transformed_df = pd.melt(df, id_vars=cols[:6], value_vars=cols[6:])
+print(transformed_df)
+        
