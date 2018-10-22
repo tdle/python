61fdded6a83f20d0ac17ceaec5c9a08beb728c40
Thomas Dale <thomasdale92@gmail.com>
2018-10-22 11:31:58 +0100

    Updated data_export.py with solution

--- PythonChallenge/data_export.py
+++ PythonChallenge/data_export.py
@@ -1,20 +1,14 @@
-#import pandas module
+"""
+    Transforms Data into Desired Format
+"""
+
 import pandas as pd
 
-#create dataframe variable of csv file I want to read
 df = pd.read_csv('data.csv')
 
-#Perform melt
-pd.melt(df,
-        id_vars=df.columns[:-2],
-        value_vars=df.columns[-2:],
-        var_name='Date',
-        value_name='Value')
+cols = df.columns
+transformed_df = pd.melt(df,
+                         id_vars=cols[:6],
+                         value_vars=cols[6:])
+print(transformed_df)
 
-
-
-#print table contents to assert code is working
-
-#write new data to table
-
-#print new table to confirm code is working
