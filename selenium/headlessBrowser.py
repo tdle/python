40426f57f8dbfd21bb82964c1eaa307c16bd3ab6
Thomas Dale <thomasdale92@gmail.com>
2018-11-04 02:17:57 +0000

    Committing files for selenium work

---
+++ selenium/headlessBrowser.py
@@ -0,0 +1,11 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+
+opts = Options()
+opts.set_headless()
+
+assert opts.headless
+browser = Firefox(options=opts)
+browser.get('http://duckduckgo.com')
+
+
